5dd57c3d0b7b760ed324811433f8b70c5be79135
Vivek Karne <vivekkarne1997@gmail.com>
2021-05-07 01:51:22 -0700

    Reviews schema

--- src/models.py
+++ src/models.py
@@ -15,37 +15,52 @@
 #     author = Column(String)
 #     pages = Column(Integer)
 #     published = Column(Date)
-    
+
 #     def __repr__(self):
 #         return "<Book(title='{}', author='{}', pages={}, published={})>"\
 #                 .format(self.title, self.author, self.pages, self.published)
 
 from flask_sqlalchemy import SQLAlchemy
- 
+
 db = SQLAlchemy()
- 
+
+
+class Reviews(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    review = db.Column(db.String(), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    user = db.relationship("User", back_populates="products")
+    product = db.relationship("Product", back_populates="users")
+
+
 class Product(db.Model):
     __tablename__ = 'products'
- 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(), nullable = False)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
     description = db.Column(db.String())
- 
+    users = db.relationship("Reviews", back_populates = "product")
+
     def __init__(self, name, description):
         self.name = name
         self.description = description
- 
+
     def __repr__(self):
         return f"{self.name}:{self.description}"
 
+
 class User(db.Model):
     __tablename__ = 'users'
- 
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(), nullable = False)
- 
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    products = db.relationship("Reviews", back_populates = "user")    
+
     def __init__(self, name):
         self.name = name
- 
+
     def __repr__(self):
         return f"{self.name}"
