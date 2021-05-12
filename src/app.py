27be9ee165e66f64a51f2599b5b84dc47fd49c74
Vivek Karne <vivekkarne1997@gmail.com>
2021-05-11 17:30:12 -0700

    Cleanup - 2

--- src/app.py
+++ src/app.py
@@ -19,33 +19,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
- 
- 
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if request.method == 'GET':
-        return "Login via the login Form"
-     
-    if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        new_product = Product(name=name, description=description)
-        db.session.add(new_product)
-        db.session.commit()
-        return f"Done!!"
-
-@app.route('/user/<user_name>', methods = ['PUT'])
-def put_user(user_name):
-   if request.method == 'PUT':
-      new_user = User(name=user_name)
-      db.session.add(new_user)
-      db.session.commit()
-      return f"User Added!"
-
-# Actual APIs
 
 @app.route('/product/<product_id>', methods = ['GET'])
 def get_product(product_id):
