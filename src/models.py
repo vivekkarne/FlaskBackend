8f2d30fdc29955a25148e0345309b09a7e7fa144
Vivek Karne <vivekkarne1997@gmail.com>
2021-05-19 00:51:46 -0700

    refactor models.py

--- src/models.py
+++ src/models.py
@@ -1,25 +1,5 @@
 # ### models.py ###
 
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Column, Integer, String, Date
-
-# Base = declarative_base()
-
-# def hi():
-#    print("H1!")
-
-# class Client(Base):
-#     __tablename__ = 'client'
-#     id = Column(Integer, primary_key=True)
-#     title = Column(String)
-#     author = Column(String)
-#     pages = Column(Integer)
-#     published = Column(Date)
-
-#     def __repr__(self):
-#         return "<Book(title='{}', author='{}', pages={}, published={})>"\
-#                 .format(self.title, self.author, self.pages, self.published)
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
